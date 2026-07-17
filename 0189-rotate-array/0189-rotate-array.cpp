fdf8ba75c9f3d2131d13f6d3e384a94f6ea961e2
Pranaov naath S P <151626056+Pranaov5925@users.noreply.github.com>
2026-07-17 22:51:28 +0530

    Time: 0 ms (100%), Space: 29.4 MB (94.4%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,19 +3,9 @@ public:
     void rotate(vector<int>& nums, int k) {
         if(nums.empty()) return;
 
-        k = k % nums.size();
-        vector<int> temp(k);
-
-        for(int i=k;i>0;i--){
-            temp[k - i] = nums[nums.size()-i];
-        }
-
-        for(int i=nums.size() - k - 1;i>=0;i--){
-            nums[i + k] = nums[i];
-        }
-
-        for(int i=0;i<k;i++){
-            nums[i] = temp[i];
-        }
+        k %= nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
     }
 };
